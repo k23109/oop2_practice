bc32a2dd034aeea95a26f56bb0513604967c48e2
k23109 <k23109kk@aitech.ac.jp>
2024-11-28 13:46:40 +0900

    Git->Hub

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import modules.SayHello as App  # Ensure the path is correct and `modules` is in your PYTHONPATH.
 
 def run():
-    app = App.SayHello("Git")
+    app = App.SayHello("GitHub")
     app.say()
 
 if __name__ == '__main__':  # Fixed spacing and syntax
